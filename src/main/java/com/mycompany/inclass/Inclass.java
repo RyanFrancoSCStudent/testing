e6fb14c359f98b78b912e5d8f93d8fdf83dec948
RyanFrancoSCStudent🐻 <ryanfrancomx@outlook.com>
2025-10-02 08:10:58 -0400

    version3

---
+++ src/main/java/com/mycompany/inclass/Inclass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inclass;
+
+/**
+ *
+ * @author ryanf
+ */
+public class Inclass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! this is version 1 ");
+    }
+}
